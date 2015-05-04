b59e25eea146240a4c1c6cc001e0130b09104048
MichalDe <odes46@gmail.com>
2015-05-04 19:50:36 +0200

    login bug fixed

--- src/main/java/pl/edu/agh/siwpia/neo4j_web_app/controllers/LoginController.java
+++ src/main/java/pl/edu/agh/siwpia/neo4j_web_app/controllers/LoginController.java
@@ -3,18 +3,17 @@ package pl.edu.agh.siwpia.neo4j_web_app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
 
-	@RequestMapping(value = "/fail", method = RequestMethod.GET)
-	public String fail(@RequestParam("password") String password, @RequestParam("email") String email) {
+	@RequestMapping(value = "/failed", method = RequestMethod.GET)
+	public String fail() {
 		return "login_failed";
 	}
 
-	@RequestMapping(value = "/page")
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
 	public String page() {
 		return "login_page";
 	}
