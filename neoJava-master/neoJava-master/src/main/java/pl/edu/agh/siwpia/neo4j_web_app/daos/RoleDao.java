37213bc5013179e233a89c366d43799dbcd32dd3
LW <lukasz.wicher25@gmail.com>
2015-05-17 12:04:14 +0200

    clean up

--- neoJava-master/neoJava-master/src/main/java/pl/edu/agh/siwpia/neo4j_web_app/daos/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.siwpia.neo4j_web_app.daos;
-
-import pl.edu.agh.siwpia.neo4j_web_app.entities.Role;
-
-public interface RoleDao extends AbstractDao<Role> {
-
-}
--- neoJava-master/neoJava-master/src/main/java/pl/edu/agh/siwpia/neo4j_web_app/services/RoleService.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.agh.siwpia.neo4j_web_app.services;
-
-import pl.edu.agh.siwpia.neo4j_web_app.entities.Role;
-
-public interface RoleService extends AbstractService<Role> {
-
-}
